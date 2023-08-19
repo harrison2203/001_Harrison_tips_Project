6d53ebf165c96f76e149d518d2b4decf3de3ec4a
Harrison <harrison.alzate-montes@epitech.eu>
2023-08-19 16:41:14 +0200

    [ADD]update

--- tips_Project/src/components/AbbrevNameComponent.vue
+++ tips_Project/src/components/AbbrevNameComponent.vue
@@ -42,7 +42,7 @@ function getInitials() {
         </div>
 
         <div>
-            <label for="reverse-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">your reversed name:</label>
+            <label for="reverse-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Result:</label>
             <input id="reverse-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required
             v-model="abbreviatedName"
 			/>
--- tips_Project/src/components/EraseVowelsComponent.vue
+++ tips_Project/src/components/EraseVowelsComponent.vue
@@ -3,11 +3,12 @@ import { ref } from 'vue';
 
 const inputString = ref('');
 const result = ref('');
+let count = ref(0);
 
 function eraseVowel (){
-
-   result.value = inputString.value.replace(/[aeiou]/gi, '');
-
+	
+	result.value = inputString.value.replace(/[aeiou]/gi, '');
+	count.value ++;
 }
 
 </script>
@@ -32,6 +33,7 @@ function eraseVowel (){
             v-model="result"
             />
         </div>
+		<div>{{ count }}</div>
 
         <button type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         @click="eraseVowel"
